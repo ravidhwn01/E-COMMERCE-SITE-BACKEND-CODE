7b665bde1ee7c98647b8ebbf06da0729021e8872
ravi kumar dhawan <ravidhawan8899@gmail.com>
2022-10-28 19:28:46 +0530

    controllers,routes setup

--- backend/app.js
+++ backend/app.js
@@ -1,3 +1,8 @@
 const express  = require("express");
 const app = express();
+// for using api
+app.use(express.json());
+//route imports
+const product = require("./routes/productRoute");
+app.use("/api/v1",product);
 module.exports = app;
---
+++ backend/controllers/productController.js
@@ -0,0 +1,4 @@
+exports.getAllProducts = (req,res)=>{
+
+    res.status(200).json({message:"route is working  fine"})
+}
