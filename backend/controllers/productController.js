72ad81615f3563cb49eed1cc28ff547ccc20081e
ravi kumar dhawan <ravidhawan8899@gmail.com>
2022-10-29 15:27:00 +0530

    get data, update data, and delete data from database

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -1,6 +1,6 @@
 const Product = require("../models/productModel");
 
-// create product
+// create product -- admin ke liye
 
 exports.createProduct = async (req, res, next) => {
   const product = await Product.create(req.body);
@@ -13,6 +13,61 @@ exports.createProduct = async (req, res, next) => {
   });
 };
 
-exports.getAllProducts = (req, res) => {
-  res.status(200).json({ message: "route is working  fine" });
+// get all  product
+exports.getAllProducts = async (req, res) => {
+  const products = await Product.find();
+  res.status(200).json({
+    status: true,
+
+    products,
+  });
 };
+
+// update product by id  --- admin ke liye
+exports.updateProduct = async (req, res) => {
+  let product = await Product.findById(req.params.id);
+  if (!product) {
+    res.status(404).json({
+      status: false,
+      message: "product not found",
+    });
+  }
+  product = await Product.findByIdAndUpdate(req.params.id, req.body, {
+    new: true,
+    runValidators: true,
+    useFindAndModify: false,
+  });
+  res.status(200).json({
+    status: true,
+    product,
+  });
+}
+// delete the product  --admin ke liye
+exports.deleteProduct = async (req, res) => {
+  const product = await Product.findById(req.params.id);
+  if (!product) {
+    res.status(404).json({
+      status: false,
+      message: "product not found",
+    });
+  }
+  await product.remove();
+  res.status(200).json({
+    status: true,
+    message: "product deleted",
+  });
+}
+//get single product
+exports.getSingleProduct = async (req, res,next) => {
+  const product = await Product.findById(req.params.id);
+  if (!product) {
+    res.status(404).json({
+      status: false,
+      message: "product not found",
+    });
+  }
+  res.status(200).json({
+    status: true,
+    product,
+  });
+}
