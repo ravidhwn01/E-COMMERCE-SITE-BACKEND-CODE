fb21ae49c923d2c13b9d555d8d0a5ba5f9d03769
ravi kumar dhawan <ravidhawan8899@gmail.com>
2022-11-08 16:57:02 +0530

    get single user and get logged  in user order feature added

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -33,3 +33,32 @@ exports.newOrder = catchAsyncError(async (req, res, next) => {
     });
     }
 );
+
+
+//get single order
+exports.getSingleOrder = catchAsyncError(async (req, res, next) => {
+    //joins in mongo   user wale collection m se user ke name or email le lega uski id ke reference se
+    const order = await Order.findById(req.params.id).populate("user", "name email");
+    
+    if (!order) {
+        return next(new ErrorHandler("No order found with this ID", 404));
+    }
+    
+    res.status(200).json({
+        success: true,
+        order,
+    });
+}
+);
+
+//get  logged in user orders
+exports.myOrders = catchAsyncError(async (req, res, next) => {
+    // ye  order me se user ki ID wale sare orders le lega
+    const orders = await Order.find({ user: req.user.id });
+    
+    res.status(200).json({
+        success: true,
+        orders,
+    });
+}
+);
--- backend/routes/orderRoute.js
+++ backend/routes/orderRoute.js
@@ -13,7 +13,11 @@ const {
 const { isAuthenticationUser, authorizeRoles } = require("../middleware/auth");
 router.route("/order/new").post(isAuthenticationUser,newOrder);
 
+// get single order
+router.route("/order/:id").get(isAuthenticationUser,authorizeRoles("admin"),getSingleOrder);
 
+// get logged in user orders
+router.route("/orders/me").get(isAuthenticationUser ,authorizeRoles("admin"),myOrders);
 
 
 //exports
