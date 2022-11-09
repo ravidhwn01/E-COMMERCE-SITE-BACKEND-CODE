6c2661fec2b26131b2bf89d6e0b5919c0a6755ea
ravi kumar dhawan <ravidhawan8899@gmail.com>
2022-11-09 15:56:40 +0530

    header of site

---
+++ frontend/src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Header from "./components/Header.js";
+import { BrowserRouter as Router } from "react-router-dom";
+
+function App() {
+  return (
+    <Router>
+      <Header />
+    </Router>
+  );
+}
+export default App;
---
+++ frontend/src/index.js
@@ -0,0 +1,7 @@
+// import react and reactdom
+import React from "react";
+import ReactDOM from "react-dom";
+// import app
+import App from "./App";
+
+ReactDOM.render(<App />, document.getElementById("root"));
