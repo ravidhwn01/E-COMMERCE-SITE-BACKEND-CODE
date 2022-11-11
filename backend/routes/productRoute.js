701414e9a1ea77c5dc2c83ba43ad1fe2f2f33367
ravi kumar dhawan <ravidhawan8899@gmail.com>
2022-11-11 13:19:36 +0530

    minor changes (isAdmin feature added)

--- backend/routes/productRoute.js
+++ backend/routes/productRoute.js
@@ -6,8 +6,8 @@ const router = express.Router();
 // we are creating routes 
 router.route("/products").get(getAllProducts);
 router.route("/admin/product/new").post(isAuthenticationUser,authorizeRoles("admin"),createProduct);    // admin ke liye
-router.route("/admin/product/:id").put(isAuthenticationUser,updateProduct);      // admin ke liye
-router.route("/admin/product/:id").delete(isAuthenticationUser, deleteProduct);   // admin ke liye
+router.route("/admin/product/:id").put(isAuthenticationUser, authorizeRoles("admin"), updateProduct);      // admin ke liye
+router.route("/admin/product/:id").delete(isAuthenticationUser, authorizeRoles("admin"),  deleteProduct);   // admin ke liye
 router.route("/product/:id").get(getSingleProduct);   // admin ke liye
 // reviews
  router.route("/review").put(isAuthenticationUser,createProductReview);   // admin ke liye
