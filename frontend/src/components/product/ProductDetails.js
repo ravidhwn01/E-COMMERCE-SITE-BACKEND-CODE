6cd72ff8903b022f9a7d94c2b4c4ac0b84574ef0
ravi kumar dhawan <ravidhawan8899@gmail.com>
2022-11-17 01:20:20 +0530

    using redux creating product details component

--- frontend/src/components/product/ProductDetails.js
+++ frontend/src/components/product/ProductDetails.js
@@ -1,11 +1,44 @@
-import React from 'react'
+import React, { useEffect } from "react";
+// css file
+// Path: frontend\src\components\product\ProductDetails.css
+import "../product/ProductDetails.css";
+//import carousel
+import Carousel from "react-material-ui-carousel";
+//import useSelector and useDispatch
+import { useSelector, useDispatch } from "react-redux";
+import {getProductDetails} from "../../actions/productAction"
+function ProductDetails(match) {
+    const dispatch = useDispatch();
+    // useEffect
+    useEffect(() => {
+        dispatch(getProductDetails(match.params.id));
+    }, [dispatch, match.params.id]);
+    // useSelector
+    const { loading, error, product } = useSelector((state) => state.productDetails);
+  
 
-function ProductDetails() {
   return (
     <>
-       
+      <div className="ProductDetails">
+        <div>
+          <Carousel>
+            {product.images &&
+              product.images.map((item , i) => (
+                <img
+                className = "CarouselImage"
+                  key={item.public_id}
+                    src={item.url}
+                    alt={product.title}
+                    height="500"
+                    width="500"
+                />
+                ))}
+
+          </Carousel>
+        </div>
+      </div>
     </>
-  )
+  );
 }
 
-export default ProductDetails
+export default ProductDetails;
